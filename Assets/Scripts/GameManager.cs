051a69196f54953d567a6c5183d62a160f761a02
Gabriel Marquesi <gabriel.mmarquesi@gmail.com>
2020-04-06 00:30:21 -0300

    First Commit
    Primeira parte completa

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameHasEnded = false;
+    public ScoreText score;
+    public float restartDelay = 0;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel() {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame() {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GameOver");
+            score.enabled = false;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
